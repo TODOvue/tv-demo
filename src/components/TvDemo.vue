9d7ecede094b3ce3f9dc959dd00d7d43cf308ba9
Cristhian Daza <cristian.d.b@hotmail.com>
2025-02-22 23:18:09 -0500

    Add component TvDemo (#1)
    * feat(theme): add global styles and theme switching functionality with SCSS
    * feat(styles): refactor SCSS to use mixins and variables for improved maintainability
    * feat(config): enhance Vite configuration with library build options and CSS injection plugin
    * feat(demo): replace static content with async component for TvDemo
    * feat(readme): update TvDemo description and enhance installation and usage instructions
    * feat(changelog): add initial changelog file following Keep a Changelog format

--- src/components/TvDemo.vue
+++ src/components/TvDemo.vue
@@ -1,11 +1,86 @@
 <script setup>
+import { HighCode } from 'vue-highlight-code';
+import useDemo from '../composable/useDemo';
 
+const props = defineProps({
+  demoStyle: {
+    type: Object,
+    default: () => ({ body: {}, content: {} })
+  },
+  hideBackground: Boolean,
+  component: Object,
+  variants: Array
+});
+
+const {
+  customStyle,
+  selectedTheme,
+  selectedVariantIndex,
+  theme,
+  toggleTheme,
+  variant,
+} = useDemo(props);
 </script>
 
 <template>
+  <div :class="`${theme}-mode`" :style="customStyle.body">
+    <div
+      class="tv-demo-body"
+      :class="{ [`${theme}-mode`]: !hideBackground }"
+      :style="customStyle.content"
+    >
+      <div class="tv-demo-theme">
+        <select
+          class="tv-demo-select tv-demo-select-theme"
+          v-model="selectedTheme"
+          @change="toggleTheme"
+        >
+          <option disabled value="">Select theme</option>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+        </select>
+      </div>
 
+      <div class="tv-demo-case">
+        <div class="tv-demo-case-demo">
+          <template v-if="variants?.length">
+            <select
+              class="tv-demo-select"
+              v-model="selectedVariantIndex"
+              v-if="variants.length > 1"
+            >
+              <option
+                v-for="(variant, index) in variants"
+                :key="variant.title"
+                :value="index"
+              >
+                {{ variant.title }}
+              </option>
+            </select>
+
+            <div class="tv-demo-component">
+              <component :is="component" v-bind="variant.propsData" />
+            </div>
+
+            <HighCode
+              class="code tv-demo-code"
+              :codeValue="variant.html"
+              :theme="theme"
+              lang="html"
+              codeLines
+              :key="variant.title"
+              height="auto"
+            />
+          </template>
+          <template v-else>
+            <h1 class="tv-demo-no-component">
+              Here the different variations of the components will be shown.
+            </h1>
+          </template>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped lang="scss" src="../assets/scss/style.scss"></style>
