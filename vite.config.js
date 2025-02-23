1c4ff5bff8e79f4e83a43de8334d8bae1424f137
Cristhian Daza <cristian.d.b@hotmail.com>
2025-02-22 22:32:24 -0500

    feat(config): enhance Vite configuration with library build options and CSS injection plugin

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,30 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: "src/components/TvDemo.vue",
+      name: "TvDemo",
+      fileName: format => `todovue-demo.${format}.js`,
+      formats: ["es", "cjs"]
+    },
+    rollupOptions: {
+      external: ["vue", "vue-highlight-code"],
+      output: {
+        globals: {
+          vue: "Vue"
+        }
+      }
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler'
-      },
-    },
-  },
+        api: "modern-compiler"
+      }
+    }
+  }
 });
