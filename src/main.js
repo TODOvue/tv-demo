f11e6f343c3b51e11a8b81bf3078b764ac2154b3
Cristhian Daza <cristian.d.b@hotmail.com>
2025-09-05 22:26:13 -0500

    feat(Tvdemo): update to version with SSR compatibility and automatic CSS imports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import TvDemo from './demo/Demo.vue'
+import 'highlight.js/styles/monokai.css';
+import 'vue-highlight-code/dist/style.css';
 
 createApp(TvDemo).mount('#tv-demo')
