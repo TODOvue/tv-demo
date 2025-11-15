03dadce80e75ecd482da092efcd06180dd68f5d3
Cristhian Daza <cristian.d.b@hotmail.com>
2025-11-14 23:17:14 -0500

    feat(entry): add syntax highlighting styles for code demonstration

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,7 @@
 import TvDemo from './components/TvDemo.vue'
 import './style.scss'
+import 'highlight.js/styles/monokai.css';
+import 'vue-highlight-code/dist/style.css';
 
 (TvDemo as any).install = (app: any) => {
   app.component('TvDemo', TvDemo)
